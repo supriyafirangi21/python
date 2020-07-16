ca01cdc6dd0bc53b4e8737f923f0f30652232f8a
supriyafirangi21 <68237027+supriyafirangi21@users.noreply.github.com>
2020-07-16 13:02:35 +0530

    Add files via upload

---
+++ july 6th python-supriya firangi/assignment2.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter a value "))
+num2 = float(input("enter a value"))
+result = num1-num2
+print(f"the subtraction of no is",result)
+if num2>num1:
+    sum = num2-num1
+    print("subtract from n2 from n1")
+else:
+    sum=num1-num2
+    print("subtract from num1-num2",sum)
