2b9e5f9928a3f2e8e92270a1d60b23ab6bdc82e9
supriyafirangi21 <68237027+supriyafirangi21@users.noreply.github.com>
2020-07-16 11:46:56 +0530

    Add files via upload

---
+++ july 6th python-supriya firangi/assignment1.py
@@ -0,0 +1,5 @@
+name = input("enter the name\t")
+age = int(input(f"{name}\tenter your age\t"))
+course = input(f"{name}\t which course you want to join\t")
+contact = int(input(f"{name}\t enter your contact no\t"))
+confirmation=input(f"{name} the above information is correct if then type yes or else no\t")
