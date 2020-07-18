4e46f7c9dd47f76b7c1c61ab88cc068aa1538eb5
supriyafirangi21 <68237027+supriyafirangi21@users.noreply.github.com>
2020-07-18 12:20:15 +0530

    Add files via upload

---
+++ july 6th python-supriya firangi/assignment3.py
@@ -0,0 +1,8 @@
+username="supriya"
+pas=1234
+uname=input("enter the user")
+password=input("enter password")
+if uname in username and password in str(pas):
+    print(f"hello {uname}")
+else:
+    print("invalid username and passsword ")
