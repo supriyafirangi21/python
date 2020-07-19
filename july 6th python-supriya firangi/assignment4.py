0cf4821b5032627bcf5912668f47a63e38e06362
supriyafirangi21 <68237027+supriyafirangi21@users.noreply.github.com>
2020-07-19 22:32:00 +0530

    Add files via upload

---
+++ july 6th python-supriya firangi/assignment4.py
@@ -0,0 +1,20 @@
+uname = input("Please enter username :")
+password = int(input("Please enter password :"))
+a1 = "supriya"
+a2 = 123451
+b1 = "priya"
+b2 = 123452
+c1 = "riya"
+c2 = 123453
+d1 = "seeta"
+d2 = 123454
+if uname == a1 and password == a2 :
+    print(f"Hello {a1} welcome")
+elif uname == b1 and password ==b2 :
+    print(f"Hello {b1} welcome")
+elif uname == c1 and password == c2 :
+    print(f"Hello {c1} welcome")
+elif uname == d1 and password == d2 :
+    print(f"Hello {d1} welcome")
+else :
+    print("The username or password you have entered is incorrect")
